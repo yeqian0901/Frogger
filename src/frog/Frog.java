2abc3f431a335b661c39c9f78e08877881b7fd24
yhe <irishe2804@gmail.com>
2021-11-16 15:55:55 +0100

    Frog

---
+++ src/frog/Frog.java
@@ -0,0 +1,42 @@
+package frog;
+
+import gameCommons.Game;
+import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
+
+public class Frog implements IFrog {
+	
+	private Game game;
+	private Case position;
+	private Direction direction;
+
+
+    public Frog(Game game) {
+        this.game = game;
+        position = new Case(game.width/2, 0);
+        direction=Direction.up;
+    }
+
+    @Override
+    public Case getPosition() {
+        return position;
+    }
+
+    @Override
+    public Direction getDirection() {
+        return direction;
+    }
+
+    @Override
+    public void move(Direction key) {
+        if(key == Direction.up && position.ord < game.height-1){
+            position = new Case(position.absc, position.ord + 1);
+    }else if(key == Direction.left){
+
+        }else if(key == Direction.right){
+
+        }else if(key == Direction.down && position.ord > 0){
+            position = new Case(position.absc, position.ord - 1);
+        }
+}
